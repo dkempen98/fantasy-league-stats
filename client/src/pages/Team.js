52ce62a5ba36b65896d36c08eaf3fb33bb4dbb7d
Drew Kempen <drewkempen98@gmail.com>
2022-11-07 23:31:56 -0500

    Add week 9

--- client/src/pages/Team.js
+++ client/src/pages/Team.js
@@ -9,7 +9,7 @@ import LineChart from "../components/reusable-stuff/lineChart.js";
 
 export default function Home() {
     const [year, setYear] = useState(2022)
-    const [week, setWeek] = useState(7)
+    const [week, setWeek] = useState(8)
     const [players, setPlayers] = useState(twentyTwoPlayers)
     const [teams, setTeams] = useState(twentyTwoTeams)
     const [defaultNames, setDefaultNames] = useState(["Alex", "Ben", "Tony", "Nate", "Henry", "Eric", "Ivan", "Trap", "Drew", "Joey"])
@@ -249,7 +249,7 @@ export default function Home() {
                         <option key={6} value={5}>Week 6</option>
                         <option key={7} value={6}>Week 7</option>
                         <option key={8} value={7}>Week 8</option>
-                        {/* <option key={9} value={8}>Week 9</option> */}
+                        <option key={9} value={8}>Week 9</option>
                         {/* <option key={10} value={9}>Week 10</option> */}
                         {/* <option key={11} value={10}>Week 11</option> */}
                         {/* <option key={12} value={11}>Week 12</option> */}
