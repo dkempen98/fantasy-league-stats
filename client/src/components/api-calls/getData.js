60e15f55bd8fe3171828f4b80eec54fb44693ce2
Drew Kempen <drewkempen98@gmail.com>
2023-03-24 18:20:07 -0400

    Move some items to an env file

--- client/src/components/api-calls/getData.js
+++ client/src/components/api-calls/getData.js
@@ -1,14 +1,15 @@
 import pkg from 'espn-fantasy-football-api/node.js';
 const { Client } = pkg;
 import fs from 'fs'
+import * as dotenv from 'dotenv'
+dotenv.config()
 
-// const myLeague = 1156809923
-const myS2 = 'AECVy%2FQ6idn3xVdL4ZHI9KOU1bO1WR6vaUFXGnDPgWsyBykDLDoCB%2BGig8vmKLcPlRYy0eX7ZfdlO4dTyi1qK4eg0dHzVYNVj9urCsdfneaB3GnvO%2FR1BwUJOdGg40wgmuOh%2BHDM8afdsftRHvwBCWiNvZsiDxRc4SPI0XRL9BWUmiasUGPlOX5vPBpCS6TPWnszQDvtwaPy2zbXQ44GqzDLSZZUTWTLZm%2FzyM6OvS%2BeVfCsL2WH8iszk7z1VLgkNANTWA%2B1eohySymXjATrnj3%2FtBpz4lswBhPhOqnEu4iijg%3D%3D'
-const mySWID = '{44B1905F-93AB-4B92-B190-5F93ABFB9218}'
+console.log("----------------API-----------------")
+console.log(process.env.LEAGUE_ID)
 
-const myClient = new Client({ leagueId: 1156809923 })
+const myClient = new Client({ leagueId: process.env.LEAGUE_ID })
 
-myClient.setCookies({ espnS2: myS2, SWID: mySWID })
+myClient.setCookies({ espnS2: process.env.S2, SWID: process.env.SWID })
 // console.log(myClient)
 
 // Team ID correlation is as follows by person:
@@ -296,7 +297,7 @@ function getInfo(week, matchupId) {
             }
             getInfo((week + 1), (matchupId + 1))
         } else {
-            console.log('-------------------ARRAY------------------')
+            // console.log('-------------------ARRAY------------------')
             // console.log(weeklyTeamData)
             // console.log(weeklyPlayerData)
             
