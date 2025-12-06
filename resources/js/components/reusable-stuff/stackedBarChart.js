b5147a41ed008708e43493c297ec3d0f798fb73c
dkempen98 <drewkempen98@gmail.com>
2025-12-06 14:21:25 -0500

    Merge in changes from master

---
+++ resources/js/components/reusable-stuff/stackedBarChart.js
@@ -0,0 +1,25 @@
+import React, { useSyncExternalStore } from "react";
+import { Bar } from 'react-chartjs-2';
+
+export default function StackedBarChart({chartData}) {
+    return <Bar data={chartData} options={ {
+        indexAxis: 'y',
+        elements: {
+            bar: {
+                width: 2,
+            }
+        },
+        maintainAspectRatio: false,
+        plugins: {
+            legend: {
+                display: false,
+            }
+        },
+        scales: {
+            y: {
+                stacked: true,
+            }
+        }
+    }
+    }/>
+}
